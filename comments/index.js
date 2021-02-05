67a2051ad3860d72b195ece3e493d20e69bceb83
Lorant Domokos <domokos.lorant@yahoo.com>
2021-02-04 19:16:56 -0800

    Comments api

---
+++ comments/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+    const postId = req.params.id;
+    const comments = commentsByPostId[postId] ?? [];
+    res.send(comments);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+    const postId = req.params.id;
+    const commentId = randomBytes(4).toString("hex");
+    const { content } = req.body;
+    const comment = { postId, content };
+
+    const comments = commentsByPostId[postId] ?? [];
+    comments.push(comment);
+    commentsByPostId[postId] = comments;
+
+    res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+    console.log("Listening on port 4001");
+});
